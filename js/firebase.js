e89d9df22d1ce02d424b783ba2658a492f48eca8
parsonsjo1 <jopa.d.parsons@gmail.com>
2016-10-05 21:59:37 -0600

    functional and pretty

--- js/firebase.js
+++ js/firebase.js
@@ -8,8 +8,7 @@ var config = {
 firebase.initializeApp(config);
 // https://firebase.google.com/docs/reference/js/firebase.database.Reference
 var chattyref = firebase.database().ref('chattyJoshy/'); 
-//default
-var sender = "parsonsjo1";
+
 moment().format();
 
 // from 
@@ -21,34 +20,15 @@ function capitalizeFirst(str)
 
 function addtweet (message) 
 {
-	// get a random user. 
-	// note something important here.  I put the code to write the
-	// tweet inside the callback for the ajax request.  
-	// this is simplistic, but it makes sure that I've got the 
-	// random person back before I try to write to the database. 
-	//$.ajax({
-	//	url: 'https://randomuser.me/api/?nat=us,gb,fr,br,es',
-	//	dataType: 'json',
-	//	success: function(data) {
-	//		console.log (data);
-	//		var name = data.results[0].name; 
-	//		var randomUserName = name.first + " " + 
-	//							 name.last;
-	//		var location = data.results[0].location; 
-
-	//		console.log('name is '+ randomUserName);
-
 	console.log ("writing"); 
 	// https://firebase.google.com/docs/reference/js/firebase.database.Reference#push
 	var newTweetRef = chattyref.push(); 
 	var timeStamp = moment().format('MMMM Do YYYY, h:mm:ss a');
 	newTweetRef.set ({
 		'message': message, 
-		'sender': sender,
+		'sender': $("#input_sender").val(),
 		'timeStamp' : timeStamp});
 	console.log ("written at: " + timeStamp); 
-	//	}
-	//});
 }
 
 // see https://firebase.google.com/docs/database/web/retrieve-data
@@ -58,22 +38,43 @@ function startTweetListener ()
 			var sender = tweet.val().sender;
 			var message = tweet.val().message; 
 			var timeStamp = tweet.val().timeStamp;
-			$("#tweetlist").append('<div class="row tweets"><div class="col-xs-6 col-xs-offset-3"><p><b>' + sender + '</b><br>tweeted: '+ message +'<br>' + timeStamp + '</p></div></div>'); 
+			$("#tweetlist").prepend(
+										'<div class="col-xs-6 col-xs-offset-3 tweet">' +
+											'<p><div class="col-xs-12" id="sender">' + sender + '</div>' +
+											'<div class="col-xs-10 col-xs-offset-1" id="message">'+ message + '</div>' +
+											'<div class="col-xs-6 col-xs-offset-7" id="timeStamp">' + timeStamp + '</div></p>' +
+											'<div class="row">' +
+												'<div class="col-xs-4 icons">' +
+													'<span class="fa fa-reply" aria-hidden="true"></span>' +
+												'</div>' + 
+												'<div class="col-xs-4 icons">' +
+													'<span class="fa fa-heart" aria-hidden="true"></span>' +
+												'</div>' +
+												'<div class="col-xs-4 icons">' +
+													'<span class="fa fa-trash" aria-hidden="true"></span>' +
+												'</div>' +
+											'</div>' +
+										'</div>'
+
+									); 
 	}); 
 }
 	
 $( document ).ready(function() {
 	startTweetListener (); 
-	$('#input_tweet').keypress(function(event) {
+
+	$("#input_tweet").focus();
+	//Hide tweet box
+	$(".more_info").hide();
+
+	//User pressed the enter key while in the input box
+	$('#tweetbox').keypress(function(event) {
 		console.log($("#input_tweet").val());
 
 		if(event.key == "Enter")
 		{
-			//set user command
-			if($("#input_tweet").val().substring(0, 9) == "set_user:")
-				sender = $("#input_tweet").val().substring(9, $("#input_tweet").val().length);
 			//clear command
-			else if($("#input_tweet").val() == "clear_admin")
+			if($("#input_tweet").val() == "admin_clear")
 			{
 				$("#tweetlist").empty();
 				chattyref.set({});
@@ -86,4 +87,30 @@ $( document ).ready(function() {
 		// because the random name generator might take some time to respond.
 		//addtweet ($('#input_tweet').val()); 
 	})
+
+	//User activated the input box
+	$("#input_tweet").focus(function(event) {
+		$(".more_info").slideDown(1000);
+	});
+
+	//Hide element when focus is lost
+	$("#tweetbox").focusout(function(event) {
+		setTimeout(function() {
+			var focus = $(document.activeElement);
+			if(focus.is(".tweetbox_child") || $(".tweetbox_child").has(focus).length)
+			{
+
+			}
+			else
+			{
+				$(".more_info").slideUp(1000);
+			}
+		}, 0);	
+	});
+
+	//User pushed the delete button
+
+	//User pushed the thumbs up
+
+	//User pushed the thumbs down
 }); 
